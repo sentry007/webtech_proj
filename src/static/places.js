66ac93855fc7a172ae720f20a0992241b65f4477
sentry007 <palwalanuzair@gmail.com>
2022-11-11 21:24:03 +0530

    project v1.6

---
+++ src/static/places.js
@@ -0,0 +1,32 @@
+const places = [
+  {
+    title: 'B6-201',
+    description:
+      "double occupancy",
+    imageUrl: process.env.PUBLIC_URL + '/assets/double.jfif',
+    time: 1500,
+  },
+  {
+    title: 'B4-443',
+    description:
+      'single occupancy',
+    imageUrl: process.env.PUBLIC_URL + '/assets/single.jfif',
+    time: 1500,
+  },
+  {
+    title: 'B4-008',
+    description:
+      'double occupancy',
+    imageUrl: process.env.PUBLIC_URL + '/assets/double.jfif',
+    time: 1500,
+  },
+  {
+    title: 'B3-69',
+    description:
+      'single occupancy',
+    imageUrl: process.env.PUBLIC_URL + '/assets/single.jfif',
+    time: 1500,
+  },
+];
+
+export default places;
